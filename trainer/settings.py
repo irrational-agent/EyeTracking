83e8b97197ce4590292750ca818727ab18d72ca0
irrational-agent <irrational.agent.0@gmail.com>
2025-08-12 06:22:23 +0300

    Rewrite the training code for combined pitch/yaw
    Make it use a folder as a dataset instead of making a query to the database.
    Also make it load data gradually, by batches.
    And some other changes.

---
+++ trainer/settings.py
@@ -0,0 +1,8 @@
+
+MAX_OFFSET = 10  # Maximum pixel offset for augmentation.
+BATCH_SIZE = 32
+IMG_SIZE = (128, 128)
+INPUT_SHAPE = (128, 256, 1)
+LEARNING_RATE = 0.007
+DATASET_DIR = "./dataset"
+OUTPUT_DIR = "./res"
